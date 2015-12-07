8a1d57b161e6a618bcf400f112143d80e3551050
Yichen Huang <yichen0831@gmail.com>
2015-12-07 13:25:40 +0800

    Add Lesson6

--- core/assets/shaders/lesson6.frag
+++ core/assets/shaders/lesson6.frag
@@ -5,8 +5,41 @@ varying vec2 v_texCoords;
 uniform sampler2D u_texture;
 uniform sampler2D u_normal;
 
+uniform vec2 u_resolution;
+uniform vec3 u_lightPos;
+uniform vec4 u_lightColor;
+uniform vec4 u_ambientColor;
+uniform vec3 u_fallOff;
+
 void main(){ 
 
+	vec4 diffuseColor = texture2D(u_texture, v_texCoords);
 	vec3 normal = texture2D(u_normal, v_texCoords).rgb * 2.0 - 1.0;
-	gl_FragColor = v_color * texture2D(u_texture, v_texCoords) * vec4(normal, 1.0);
+
+	vec3 lightDir = vec3(u_lightPos.xy - (gl_FragCoord.xy / u_resolution.xy), u_lightPos.z);
+	
+	// correct for aspect ratio
+	lightDir.x *= u_resolution.x / u_resolution.y;
+	
+	// the distance 
+	float D = length(lightDir);
+	
+	// normalize the vectors
+	vec3 N = normalize(normal);
+	vec3 L = normalize(lightDir);
+	
+	// pre-multiply light color with intensity
+	// then perform N dot L to determine the diffuse term
+	vec3 diffuse = (u_lightColor.rgb * u_lightColor.a) * max(dot(N, L), 0.0);
+	
+	// pre-multiply ambient color with intensity
+	vec3 ambient = u_ambientColor.rgb * u_ambientColor.a;
+	
+	// calculate the attenuation
+	float attenuation = 1.0 / (u_fallOff.x + (u_fallOff.y * D) + (u_fallOff.z * D * D)); 
+	
+	vec3 intensity = ambient + diffuse * attenuation;
+	vec3 finalColor = diffuseColor.rgb * intensity;
+
+	gl_FragColor = v_color * vec4(finalColor, diffuseColor.a);
 }
--- core/src/com/ychstudio/screens/Lesson6.java
+++ core/src/com/ychstudio/screens/Lesson6.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Vector3;
 import com.ychstudio.Tutorial;
 
 import utils.FileUtils;
@@ -23,18 +24,29 @@ public class Lesson6 implements Screen {
     
     private ShaderProgram shader;
     
+    private static final float DEFAULT_LIGHT_Z = 0.075f;
+    private static final float LIGHT_INTENSITY = 1f;
+    private static final float AMBIENT_INTENSITY = 0.2f;
+    private static final Vector3 LIGHT_COLOR = new Vector3(1f, 0.8f, 0.6f);
+    private static final Vector3 AMBIENT_COLOR = new Vector3(0.6f, 0.6f, 1.0f);
+    private static final Vector3 FALLOFF = new Vector3(0.4f, 3f, 20f);
+    
+    private final Vector3 lightPos;
+    
     private final String VERT_SHADER = "basic.vert";
     private final String FRAG_SHADER = "lesson6.frag";
     
     public Lesson6(Tutorial tutorial) {
         this.tutorial = tutorial;
         batch = tutorial.getSpriteBatch();
+        
+        lightPos = new Vector3(0, 0, DEFAULT_LIGHT_Z);
     }
 
     @Override
     public void show() {
-        texture0 = new Texture("suzanne.png");
-        normalMap = new Texture("suzanne_n.png");
+        texture0 = new Texture("rock.png");
+        normalMap = new Texture("rock_n.png");
         
         shader = new ShaderProgram(VERT_SHADER, FRAG_SHADER);
         shader = new ShaderProgram(FileUtils.loadShaderFile(VERT_SHADER), FileUtils.loadShaderFile(FRAG_SHADER));
@@ -48,28 +60,49 @@ public class Lesson6 implements Screen {
 //      ShaderProgram.pedantic = false;
         
         shader.begin();
-        shader.setUniformi("u_normal", 1);
+        shader.setUniformi("u_normal", 1); // GL_TEXTURE1
+        shader.setUniformf("u_lightColor", LIGHT_COLOR.x, LIGHT_COLOR.y, LIGHT_COLOR.z, LIGHT_INTENSITY);
+        shader.setUniformf("u_ambientColor", AMBIENT_COLOR.x, AMBIENT_COLOR.y, AMBIENT_COLOR.z, AMBIENT_INTENSITY);
+        shader.setUniformf("u_fallOff", FALLOFF);
+        shader.setUniformf("u_resolution", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         shader.end();
         
-        normalMap.bind(1);
         
-        Gdx.gl.glActiveTexture(GL20.GL_TEXTURE0);
-        
-        lessonStage = new LessonStage(tutorial);
+      lessonStage = new LessonStage(tutorial);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        float x = Gdx.input.getX() / (float) Gdx.graphics.getWidth();
+        float y = Gdx.input.getY() / (float) Gdx.graphics.getHeight();
+        lightPos.set(x, 1 - y, DEFAULT_LIGHT_Z);
+        
+        shader.begin();
+        shader.setUniformf("u_lightPos", lightPos);
+        shader.end();
      
+        batch.setShader(shader);
+        batch.begin();
+        //bind normal map to texture unit 1
+        normalMap.bind(1);
+        //bind diffuse color to texture unit 0
+        //important that we specify 0 otherwise we'll still be bound to glActiveTexture(GL_TEXTURE1)
+        texture0.bind(0);
+        batch.draw(texture0, 0, 0, 640f, 480f);
+        batch.end();
+        batch.setShader(null);
         
         lessonStage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        
+        shader.begin();
+        shader.setUniformf("u_resolution", width, height);
+        shader.end();
     }
 
     @Override
